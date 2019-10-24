8954aa9dcef563e54fd8c8469142ffd7897ad1ad
selfvolinghis <2629106344@qq.com>
2019-10-24 09:46:04 +0800

    初始化文件提交

---
+++ src/main/java/com/ehs/EamApplication.java
@@ -0,0 +1,13 @@
+package com.ehs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EamApplication.class, args);
+	}
+
+}
