814905fee7be93fb9161e3d82bd2e4c7d9268597
Mapleaves <15229255171@163.com>
2019-10-25 09:40:11 +0800

    Create EamPartLibraryServiceImpl.java
    提交目录结构

---
+++ src/main/java/eam/eamPartLibrary/service/impl/EamPartLibraryServiceImpl.java
@@ -0,0 +1,10 @@
+package eam.eamPartLibrary.service.impl;
+
+import org.springframework.stereotype.Service;
+
+import eam.eamPartLibrary.service.EamPartLibraryService;
+
+@Service
+public class EamPartLibraryServiceImpl implements EamPartLibraryService {
+
+}
