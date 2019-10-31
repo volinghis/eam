601daf4e28e9f12cbd118798b87bb6bd554b3823
Mapleaves <15229255171@163.com>
2019-10-31 14:39:36 +0800

    Update EamPartLibraryHis.java

--- src/main/java/com/ehs/eam/eamPartLibrary/entity/EamPartLibraryHis.java
+++ src/main/java/com/ehs/eam/eamPartLibrary/entity/EamPartLibraryHis.java
@@ -3,6 +3,8 @@ package com.ehs.eam.eamPartLibrary.entity;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
+import com.ehs.eam.eamPartLibrary.entity.entitysuper.EamPartLibrary;
+
 @Entity
 @Table(name = "EAM_PART_LIBRARY_HIS")
 public class EamPartLibraryHis extends EamPartLibrary {
