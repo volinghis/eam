874d9fa7ef5624e3394c2a803724b63c6db7ce1c
Mapleaves <15229255171@163.com>
2019-10-31 10:47:43 +0800

    修改备件实体

---
+++ src/main/java/com/ehs/eam/eamPartLibrary/entity/EamPartLbraryHis.java
@@ -0,0 +1,13 @@
+package com.ehs.eam.eamPartLibrary.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "EAM_PART_LIBRARY_HIS")
+public class EamPartLbraryHis extends EamPartLibrary {
+
+	// @Fields serialVersionUID : TODO
+	private static final long serialVersionUID = 1L;
+
+}
